0a175abd419626e65113e0f559e3e394bb8a6105
Leon Gorissen <leon@_.ms.mmf.cuni.cz>
2021-10-11 10:43:38 +0200

    Feat: Dataloader function w/auto download

---
+++ csng_invariances/encoding.py
@@ -0,0 +1,14 @@
+"""Module provding CNN encoding functionality."""
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+import neuralpredictors as neur
+
+
+# Load Data
+
+
+if __name__ == "__main__":
+    pass
