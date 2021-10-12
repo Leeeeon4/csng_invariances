d12bc7922dbd1534ae66b43c00603c1d74a77969
Leon Gorissen <leon@_.ms.mmf.cuni.cz>
2021-10-12 11:19:35 +0200

    Feat: Beginning Encoder

--- csng_invariances/encoding.py
+++ csng_invariances/encoding.py
@@ -5,9 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 import neuralpredictors as neur
+from datasets.lurz2020 import get_dataloaders
 
 
 # Load Data
+dataloaders = get_dataloaders()
 
 
 if __name__ == "__main__":
