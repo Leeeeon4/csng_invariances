fd44821699505da2314e349e88e49f2143f65326
Leon Michel Gorißen <leon.gorissen@rwth-aachen.de>
2021-10-12 16:19:18 +0200

    Update: Some work on encoding model

--- csng_invariances/encoding.py
+++ csng_invariances/encoding.py
@@ -9,24 +9,26 @@ from datasets.lurz2020 import get_dataloaders
 from models.discriminator import get_core_trained_model
 from training.trainers import standard_trainer as trainer
 
-def main():
+
+def train_and_eval():
     # Load data and model
     dataloaders = get_dataloaders()
     model = get_core_trained_model(dataloaders)
 
     # If you want to allow fine tuning of the core, set detach_core to False
-    detach_core=True
+    detach_core = True
     if detach_core:
-        print('Core is fixed and will not be fine-tuned')
+        print("Core is fixed and will not be fine-tuned")
     else:
-        print('Core will be fine-tuned')
+        print("Core will be fine-tuned")
 
-    trainer_config = {'track_training': True,
-                    'detach_core': detach_core}
+    trainer_config = {"track_training": True, "detach_core": detach_core}
 
     # Run trainer
-    score, output, model_state = trainer(model=model, dataloaders=dataloaders, seed=1, **trainer_config)
+    score, output, model_state = trainer(
+        model=model, dataloaders=dataloaders, seed=1, **trainer_config
+    )
 
 
 if __name__ == "__main__":
-    main()
+    train_and_eval()
