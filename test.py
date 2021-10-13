20f89915a922984dcb0c19155632babea3c52038
Leon Michel Gorißen <leon.gorissen@rwth-aachen.de>
2021-10-13 11:16:57 +0200

    Chore: Clean up dir

--- test.py
+++
@@ -1,9 +0,0 @@
-#%%
-from csng_invariances.models.discriminator import get_core_trained_model
-from csng_invariances.datasets.lurz2020 import get_dataloaders
-
-dataloaders = get_dataloaders()
-model = get_core_trained_model(dataloaders)
-print(model)
-
-# %%
