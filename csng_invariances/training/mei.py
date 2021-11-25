5a14582123d9e48d0b5aa63f595c79c60bd6d9e5
Leon Michel Gorißen <leon.gorissen@rwth-aachen.de>
2021-11-25 12:59:12 +0100

    Feat: Naive MEI computation

--- csng_invariances/training/mei.py
+++ csng_invariances/training/mei.py
@@ -6,64 +6,127 @@ _, c, w, h = images.shape
 2. src_image = torch.zeros(1, c, w, h, requires_grad=True, device=device)
 """
 
+from pathlib import Path
+from datetime import datetime
 import torch
-from csng_invariances.data.preprocessing import *
+from rich.progress import track
+import matplotlib.pyplot as plt
+from numpy import save
 
 
-def meis(model, images, responses, neurons_to_select, octaves, seed=1, **kwargs):
+def naive_gradient_ascent_step(
+    criterion: torch.nn.Module,
+    encoding_model: torch.nn.Module,
+    image: torch.Tensor,
+    neuron_idx: int,
+    lr: float = 0.01,
+    *args: int,
+    **kwargs: int,
+) -> torch.Tensor:
+    """Performs one step of naive gradient ascent.
 
-    device = torch.device("cuda" if next(model.parameters()).is_cuda else "cpu")
+    Adds the scaled gradient to the image and return it.
 
-    _, c, w, h = images.shape
-    image_count, neuron_count = responses.shape
-    selected_neurons_count = neurons_to_select.shape
-    white_noise_image_tensor = torch.randint(
-        0, 255, size=(1, c, w, h), requires_grad=True, device=device, dtype=torch.float
-    )
-    preprocessed_white_noise_image_tensor = image_preprocessing(
-        white_noise_image_tensor
-    )
+    Args:
+        criterion (torch.nn.Module): loss function a.k.a. criterion.
+        encoding_model (torch.nn.Module): trained encoding model which is the
+            basis for the meis
+        image (torch.Tensor): gaussian white noise image in pytorch convention.
+        neuron_idx (int): neuron to analyize
+        lr (float, optional): Learning rate. Defaults to 0.01.
 
-
-# TODO compute meis for selected neurons:
-# pick gradient with respect to selected neuron
-# for neuron in range(selected neuron coutn):
-#   current mei = compute mei (neuron, white_noise_image_tensor)
-#   meis[neuron,:,:,:] = current mei
-
-#     for e, o in enumerate(octaves):
-#         for i in range(o['iter_n']):
-#             ox = 0
-#             oy = 0
-#             src.data[0].copy_(torch.Tensor(image))
-
-#             sigma = o['start_sigma'] + ((o['end_sigma'] - o['start_sigma']) * i) / o['iter_n']
-#             step_size = o['start_step_size'] + ((o['end_step_size'] - o['start_step_size']) * i) / o['iter_n']
-
-#             make_step(net, src, bias=bias, scale=scale, sigma=sigma, step_size=step_size, **step_params)
-
-#             if i % 10 == 0:
-#                 print('finished step %d in octave %d' % (i, e))
-
-#             # insert modified image back into original image (if necessary)
-#             image[:, ox:ox + w, oy:oy + h] = src.data[0].cpu().numpy()
-
-#     # returning the resulting image
-#     return unprocess(image, mu=bias, sigma=scale)
+    Returns:
+        torch.Tensor: image after one gradient ascent step.
+    """
+    # Image normalization
+    with torch.no_grad():
+        image /= image.max()
+    loss = criterion(encoding_model(image), neuron_idx)
+    loss.backward()
+    # Gradient ascent step.
+    # The scaled and then multiplied by the learning rate.
+    with torch.no_grad():
+        image += image.grad * image.max() / image.grad.max() * lr
+    image.grad = None
+    return image
 
 
-# def process(x, mu=0.4, sigma=0.224):
-#     """ Normalize and move channel dim in front of height and width"""
-#     x = (x - mu) / sigma
-#     if isinstance(x, torch.Tensor):
-#         return x.transpose(-1, -2).transpose(-2, -3)
-#     else:
-#         return np.moveaxis(x, -1, -3)
+def mei(
+    criterion: torch.nn.Module,
+    encoding_model: torch.nn.Module,
+    image: torch.Tensor,
+    selected_neuron_indicies: list,
+    lr: float = None,
+    epochs: int = 200,
+    show: bool = False,
+    *args: int,
+    **kwargs: int,
+) -> dict:
+    """Compute the MEIs.
 
-# def unprocess(x, mu=0.4, sigma=0.224):
-#     """Inverse of process()"""
-#     x = x * sigma + mu
-#     if isinstance(x, torch.Tensor):
-#         return x.transpose(-3, -2).transpose(-2, -1)
-#     else:
-#         return np.moveaxis(x, -3, -1)
+    Computes the MEIs by means of gradient ascent for the selected_neuron_indicies list and stores them.
+
+    Args:
+        criterion (torch.nn.Module): Loss function
+        encoding_model (torch.nn.Module): encoding model which is basis for MEIs
+        image (torch.Tensor): Gaussian white noise image
+        selected_neuron_indicies (list): List of neurons to compute MEIs for
+        lr (float, optional): Learning Rate. Defaults to None.
+        epochs (int, optional): Number of epochs. Defaults to 200.
+        show (bool, optional): If True, MEIs are shown after every 10 steps.
+            Defaults to False.
+
+    Returns:
+        dict: Dictionary of neuron_idx and the associated MEI.
+    """
+    meis = {}
+    t = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    meis_directoy = Path.cwd() / "data" / "processes" / "MEIs" / t
+    meis_directoy.mkdir(parents=True, exist_ok=True)
+    for counter, neuron in enumerate(selected_neuron_indicies):
+        trainer_config = {
+            "criterion": criterion,
+            "encoding_model": encoding_model,
+            "epochs": epochs,
+            "neuron_idx": neuron,
+        }
+        if lr is not None:
+            trainer_config["lr"] = lr
+        old_image = image.detach().clone()
+        old_image.requires_grad = True
+        new_image = naive_gradient_ascent_step(image=old_image, **trainer_config)
+        for epoch in track(
+            range(epochs),
+            total=epochs,
+            description=f"Neuron {counter}/{len(selected_neuron_indicies)}: ",
+        ):
+            new_image = naive_gradient_ascent_step(image=new_image, **trainer_config)
+            if show and epoch % 10 == 0:
+                fig, ax = plt.subplots(figsize=(6.4 / 2, 3.6 / 2))
+                im = ax.imshow(new_image.detach().numpy().squeeze())
+                ax.set_title(f"Image neuron {neuron} after {epoch} epochs")
+                plt.colorbar(im)
+                plt.tight_layout()
+                plt.show(block=False)
+                plt.pause(0.1)
+                plt.close()
+        save(meis_directoy / f"MEI_neuron_{neuron}.npy", new_image.detach().numpy())
+        if (meis_directoy / "readme.md").exists() is False:
+            with open(meis_directoy / "readme.md", "w") as f:
+                f.write(
+                    "# readme\n"
+                    "The MEIs (Most Exciting Images) store the pytorch 4D representation"
+                    "of the image per neuron. Dimensions are (batchsize, channels, "
+                    "height, width)."
+                )
+        meis[neuron] = new_image
+        if show:
+            fig, ax = plt.subplots(figsize=(6.4 / 2, 3.6 / 2))
+            im = ax.imshow(new_image.detach().numpy().squeeze())
+            ax.set_title(f"Final image neuron {neuron}")
+            plt.colorbar(im)
+            plt.tight_layout()
+            plt.show(block=False)
+            plt.pause(3)
+            plt.close("all")
+    return meis
